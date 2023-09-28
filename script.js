37a064be2f69bc7edbed284034f827edb7f0702e
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-09-29 00:25:24 +0200

    add : examples of variables in JS

---
+++ script.js
@@ -0,0 +1,15 @@
+// That's a great idea! Here's a quick overview of some exercises you can do to help you learn about variables in JavaScript:
+// - Create a variable and assign a value to it.
+// - Modify the value of the variable.
+// - Use the 'var' keyword to create multiple variables.
+// - Use the 'for' loop to iterate through a list of variables.
+// - Use the 'if' statement to test the value of a variable.
+// - Use the 'switch' statement to control the flow of execution based on a variable.
+// I hope this is helpful! What do you think?
+
+
+- var myName = "John Smith";
+- var myAge = 23;
+- var myFavoriteColor = "red";
+- var myHometown = "New York City";
+- var myFavoriteFood = "Pizza";
